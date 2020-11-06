1676bf441888bd343a2c81c53d0603a7cd373438
kai <kai.pinckard@gmail.com>
2020-11-06 12:55:13 -0800

    add all required functions

--- mapreduce.cc
+++ mapreduce.cc
@@ -3,12 +3,18 @@
 #include <vector>
 #include <queue>
 #include <utility>
+#include <algorithm>
+#include <mutex>
+#include "mapreduce.hh"
+#include <cassert>
 
+namespace MapReduce{
 // C++ redefinition of mapreduce.h
 
-using list_t = std::vector<std::string>;
+using list_t = std::vector<std::pair<std::string, std::string>>;
 using pair_t = std::pair<int, list_t>;
 using results_t = std::vector<pair_t>;
+using part_t = int;
 
 // global variable.
 
@@ -16,10 +22,32 @@ using results_t = std::vector<pair_t>;
 // have a number of different values listed. all of the same key will be stored in the same
 // partition. the datastructure needs to remember the last accessed address for each partition. 
 results_t mapped_results;
+std::queue<char*> files_queue;
+std::mutex queue_mutex;
 
-/* map is handed a file name to process and is expected to handle it in its entirety.
+void Map(const char *file_name) {
+    FILE *fp = fopen(file_name, "r");
+    assert(fp != NULL);
 
-*/
+    char *line = NULL;
+    size_t size = 0;
+    while (getline(&line, &size, fp) != -1) {
+        char *token, *dummy = line;
+        while ((token = strsep(&dummy, " \t\n\r")) != NULL) {
+            MR_Emit(token, "1");
+        }
+    }
+    free(line);
+    fclose(fp);
+}
+
+void Reduce(const std::string& key, getter_t get_next, int partition_number) {
+    int count = 0;
+    std::string value;
+    while ((value = get_next(key, partition_number)) != "")
+        count++;
+    std::cout << key << " " << count << "\n";
+}
 
 /*
 This function needs to take key value pairs from many different 
@@ -37,17 +65,35 @@ values have been processed.
 responsible for storing the key and value in shared datastructure
 hash map to arrays.
 */
-/* void MR_Emit(const std::string& key, const std::string& value)
+void MR_Emit(const std::string& key, const std::string& value)
 {
-    mapped_results[key].push_back(value);
+    int index = MR_DefaultHashPartition(key, mapped_results.size());
+    std::pair<std::string, std::string> pair(key, value);
+    mapped_results[index].second.push_back(pair);
 }
 
 
-void getter(const std::string& key, int partition_number)
+std::string getter(const std::string& key, int partition_number)
 {
-    return;
+    //int index = MR_DefaultHashPartition(key, mapped_results.size());
+    int index = partition_number;
+    list_t partition = mapped_results[index].second;
+
+    // Find the next key instance starting from previous key instance
+    for(int i = mapped_results[index].first; i < partition.size(); i++)
+    {
+        if(key.compare(partition[i].first) == 0)
+        {
+            mapped_results[index].first = i;
+            return partition[i].second;
+        }
+    }
+
+    // If there are no more occurances of the key to find
+    mapped_results[index].first += 1;
+    return "";
 }
- */
+
 /*
 This function is used by the map reduce library to decide which partition
 and hence which reducer thread gets a particular key/list of values to process. For some applications which
@@ -59,37 +105,44 @@ For each partition keys and the value list associated with said keys should be s
 thus when a particular reducer thread and its associated partition are working the reduce function should be called on each key in order for that 
 partition. 
 */
-/* unsigned long MR_DefaultHashPartition(const std::string& key, int num_partitions)
+unsigned long MR_DefaultHashPartition(const std::string& key, int num_partitions)
 {
     unsigned long hash = 5381;
     int c;
-    while ((c = *key++) != '\0')
+    for(int i = 0; i < key.length(); i++)
+    {
         hash = hash * 33 + c;
+    }
     return hash % num_partitions;
 }
- */
-/* // Need to update this to be thread safe.
+
 void mapper_pool(mapper_t map)
 {
-    while(!queue.empty())
+    std::string file_name; // possible error
+    while(true)
     {
-        char* file_name = queue.front();
-        queue.pop_front();
-        map(file_name);
+        {
+            std::lock_guard<std::mutex> lock(queue_mutex);
+            if(files_queue.empty())
+            {
+                return;
+            }
+            file_name = files_queue.front();
+            files_queue.pop();
+        }
+        map(file_name.c_str());
     }
 }
 
-void reducer_pool(reducer_t reduce)
+void reducer_manager(reducer_t reduce, int part_number)
 {
-    while(!queue.empty())
+    while(mapped_results[part_number].first < mapped_results[part_number].second.size())
     {
-        char* file_name = queue.front();
-        queue.pop_front();
-        map(file_name);
+        std::string key = mapped_results[part_number].second[mapped_results[part_number].first].first;
+        reduce(key, getter, part_number);
     }
 }
 
- */
 /*
     This function takes the command line arguments of a given program, a pointer to a
     map function, the number of mapper threads your library should create,
@@ -108,55 +161,62 @@ void reducer_pool(reducer_t reduce)
     for(int ii = 0; ii < Num_Threads; ii++)
     {  Pool.push_back(thread(Infinite_loop_function));} 
     */
-/* 
+
 void MR_Run(int argc, char* argv[], mapper_t map, int num_mappers, reducer_t reduce, int num_reducers, partitioner_t partition)
 {
-    results_t results(num_partitions);
-    
 
-    std::queue<char*> files;
+    // Place the files into the queue
     for(int i = 0; i < argc; i++)
     {
-        queue.push_back(argv[i]);
+        files_queue.push(argv[i]);
     }
+
+    // Initialize one partition for each reducer
+    for(int i = 0; i < num_reducers; i++)
+    {
+        list_t list;
+        //list.push_back("hello there");
+        pair_t pair(0, list);
+        mapped_results.push_back(pair);
+    }
+
     // start mappers
     std::vector<std::thread> mappers;
     for(int i = 0; i < num_mappers; i++)
     {
-        mappers.push_back(std::thread(map));
+        mappers.push_back(std::thread(mapper_pool, map));
     }
 
+    // wait for mappers to complete
+    for(int i = 0; i < num_mappers; i++)
+    {
+        mappers[i].join();
+    }
+
+    // sort the partitions
+    for(int i = 0; i < mapped_results.size(); i++)
+    {
+        std::sort(mapped_results[i].second.begin(), mapped_results[i].second.end());
+    }
 
     // start reducers
     std::vector<std::thread> reducers;
     for(int i = 0; i < num_reducers; i++)
     {
-        reducers.push_back(std::thread(map));
+        reducers.push_back(std::thread(reducer_manager, reduce, i));
     }
 
-
-    return;
-} */
-
-void tester()
-{
-    std::cout << mapped_results[0].second[0];
-}
-
-void creater()
-{
-  for( int i =0; i < 4; i++)
+    // wait for reducers to complete
+    for(int i = 0; i < num_reducers; i++)
     {
-        list_t list;
-        list.push_back("hello there");
-        pair_t pair(0, list);
-        mapped_results.push_back(pair);
+        reducers[i].join();
     }
 }
 
 int main(int argc, char* argv[])
 {
-    creater();
-    tester();   
+    MR_Run(argc, argv, Map, 10, Reduce, 10, MR_DefaultHashPartition);
     return 0;
+}
+
 }
