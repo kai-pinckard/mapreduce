1bf69964db5a44320898b421a66015045d92dd66
kai <kai.pinckard@gmail.com>
2020-11-08 21:19:51 -0800

    improve comments and process files in descending order by file size

--- mapreduce.cc
+++ mapreduce.cc
@@ -13,24 +13,32 @@ Completed by: Kai Pinckard
 #include "mapreduce.hh"
 #include <cassert>
 #include <fstream>
+#include <filesystem>
 #include <memory>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 namespace MapReduce
 {
+    // A list containing key value pairs.
     using list_t = std::vector<std::pair<std::string, std::string>>;
+    // A utility pair containing an index into a list of key value pairs and a mutex ptr to lock that list
+    // This index is used by get_next to keep track of which values have already been returned.
     using util_t = std::pair<long unsigned int, std::mutex*>;
+    // A pair containing a utility pair and its corresponding list of key value pairs
     using pair_t = std::pair<util_t, list_t>;
+    // The datatype for the central data structure: a list of key value pair lists each with its own utility pair.
     using results_t = std::vector<pair_t>;
-    using part_t = int;
 
-    /*
-        Mapped results stores each partition in its own vector with its own mutex ptr
-        so that different threads can access different partitions concurrently.
-    */
+    // The globally accessible central datastructure.
     results_t mapped_results;
-    // A Queue containing all input files names that need to be mapped
+
+    // A queue containing all input files names that need to be mapped
     std::queue<char*> files_queue;
     std::mutex queue_mutex;
+
     // Global partition function
     partitioner_t partition_func = MR_DefaultHashPartition;
 
@@ -42,8 +50,8 @@ namespace MapReduce
     {
         int index = partition_func(key, mapped_results.size());
         std::pair<std::string, std::string> pair(key, value);
-        // need to acquire the lock for the bucket
         {
+            // Acquire the lock for the specific list that is being accessed.
             std::lock_guard<std::mutex> lock(*(mapped_results[index].first.second));
             mapped_results[index].second.push_back(pair);
         }
@@ -56,21 +64,21 @@ namespace MapReduce
     */
     std::string getter(const std::string& key, int part_number)
     {
+        // Access the list of key value pairs corresponding to part_number
         list_t partition = mapped_results[part_number].second;
 
-        // Find the next key instance starting from previous key instance
+        // Get the next index to process from inside the current list's utility pair
         long unsigned int i = mapped_results[part_number].first.first;
+
+        // Check if there are more key value pairs to process
         if(i < partition.size())
         {
+            // If the key matches increment the stored index and return the value
             if(key.compare(partition[i].first) == 0)
             {
 
                 mapped_results[part_number].first.first= i + 1;
                 return partition[i].second; 
-            }
-            else
-            {
-                return "";
             }
         }
         return "";
@@ -120,7 +128,6 @@ namespace MapReduce
     {
         while(mapped_results[part_number].first.first < mapped_results[part_number].second.size())
         {
-            // Get the index
             int next_key_index = mapped_results[part_number].first.first;
             std::string key = mapped_results[part_number].second[next_key_index].first;
             reduce(key, getter, part_number);
@@ -138,17 +145,45 @@ namespace MapReduce
     */
     void MR_Run(int argc, char* argv[], mapper_t map, int num_mappers, reducer_t reduce, int num_reducers, partitioner_t partition=MR_DefaultHashPartition)
     {
-
-        // Update the partition global partition function
+        // Update the global partition function
         partition_func = partition;
-        
-        // Place the files into the queue
-        for(int i = 1; i < argc; i++)
+
+        // Create a list of all the file names and their sizes.
+        std::pair<long int, char*> file_info;
+        std::vector<std::pair<long int, char*>> file_list;
+        struct stat statbuffer;
+        for (int i = 1; i < argc; i++)
         {
-            files_queue.push(argv[i]);
+            int fd = open(argv[i], O_RDONLY, S_IRUSR | S_IWUSR);
+            if(fd == -1)
+            {
+                perror("Unable to open file");
+                exit(1);
+            }
+
+            if(fstat(fd, &statbuffer) == -1)
+            {
+                perror("Unable to get stat info for input file");
+                exit(1);
+            }
+
+            file_info.first = statbuffer.st_size;
+            file_info.second = argv[i];
+            file_list.push_back(file_info);
+        }
+
+        // Sort the file_list in descending order
+        // Since reduce phase does not begin until all mappers
+        // finish it is good to get started on larger map jobs early.
+        sort(file_list.begin(), file_list.end(), std::greater<std::pair<long int, char*>>()); 
+  
+        // Place the files into the queue in descending order by file length
+        for(unsigned long int i = 0; i < file_list.size(); i++)
+        {
+            files_queue.push(file_list[i].second);
         }
         
-        // Initialize datastructure
+        // Initialize central datastructure
         for(int i = 0; i < num_reducers; i++)
         {
             list_t list;
@@ -175,11 +210,11 @@ namespace MapReduce
         for(long unsigned int i = 0; i < mapped_results.size(); i++)
         {
             std::sort(mapped_results[i].second.begin(), mapped_results[i].second.end());
-            // free mutex's
             delete mapped_results[i].first.second;
         }
 
         // start reducers
+        // Since only one reducer accesses each partition no locking is needed
         std::vector<std::thread> reducers;
         for(int i = 0; i < num_reducers; i++)
         {
